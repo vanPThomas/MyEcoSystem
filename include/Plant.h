6d2495d393d5b1de9b2554c5575679cd238101cf
Thomas Van Petegem <thomas.van.petegem@hotmail.com>
2026-04-23 09:43:57 +0200

    Added plant constructor and getters

--- include/Plant.h
+++ include/Plant.h
@@ -3,9 +3,18 @@
 class Plant
 {
 public:
+    Plant(float startX, float startY);
 
+    float getHealthPoints() const {return healthPoints;}
+    float getEnergyPerHealthPoint() const {return energyPerHealthPoint;}
+    float getPoisonLevel() const {return poisonLevel;}
+    float getAge() const {return age;}
+
+    float x = 0.0f;
+    float y = 0.0f;
+    
 private:
-    int healthPoints = 100; // how much there is to eat
+    float healthPoints = 100; // how much there is to eat
     float energyPerHealthPoint = 0.5f;   // how much energy is provided to creature per eaten health point
     float poisonLevel = 5.0f;   // how dangerous this plant is to eat, out of 10.
     float age = 0.0f;
---
+++ src/World/Plant.cpp
@@ -0,0 +1,7 @@
+#include "Plant.h"
+
+Plant::Plant(float startX, float startY, int spaceWidth, int spaceHeight)
+    : x(startX)
+    , y(startY)
+{
+}
